d4161714a18e34f8e69ba6ba2c1f0f75e179e9bd
Stanley Barros Rodrigues <dev03.dlp@dolpengenharia.local>
2026-01-06 15:38:13 -0300

    fix: update config archive proxy.ts

--- src/proxy.ts
+++ src/proxy.ts
@@ -2,25 +2,23 @@ import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
 export function proxy(req: NextRequest) {
-  const token = req.cookies.get("refreshToken")?.value; // ajuste conforme o cookie de autenticação usado
+  const token = req.cookies.get("refreshToken")?.value;
+  const { pathname } = req.nextUrl;
 
-  const isAuthPage = req.nextUrl.pathname === "/";
-  const isProtectedRoute = req.nextUrl.pathname.startsWith("/home");
+  const isAuthPage = pathname === "/";
+  const isProtectedRoute = pathname.startsWith("/privates");
 
-  // Se tentar acessar rota protegida sem token → redireciona pro login
   if (isProtectedRoute && !token) {
     return NextResponse.redirect(new URL("/", req.url));
   }
 
-  // Se já estiver logado e tentar acessar o login → redireciona pra home
   if (isAuthPage && token) {
-    return NextResponse.redirect(new URL("/home", req.url));
+    return NextResponse.redirect(new URL("/privates/home", req.url));
   }
 
   return NextResponse.next();
 }
 
-// define as rotas onde o middleware será executado
 export const config = {
   matcher: ["/((?!_next/static|_next/image|favicon.ico|api/).*)"],
 };
